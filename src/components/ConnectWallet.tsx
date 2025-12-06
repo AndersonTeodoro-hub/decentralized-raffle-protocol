eabd027c7c972fa11d263f5d3414db5132cb3b19
AndersonTeodoro-hub <cris7981x@gmail.com>
2025-12-06 11:46:52 +0000

    Create ConnectWallet.tsx

---
+++ src/components/ConnectWallet.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function ConnectWallet() {
+  return (
+    <div className="flex justify-center mt-4">
+      <ConnectButton />
+    </div>
+  );
+}

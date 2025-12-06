589c88bd49da6341411788a96d54587148e05d20
AndersonTeodoro-hub <cris7981x@gmail.com>
2025-12-06 11:58:47 +0000

    Update ConnectWallet.tsx

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -4,8 +4,9 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function ConnectWallet() {
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center py-6">
       <ConnectButton />
     </div>
   );
 }
+
